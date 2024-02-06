07abb322de57cd8fee57dd9801a4ec1dd0cae5ca
ricardomarinids <159140459+ricardomarinids@users.noreply.github.com>
2024-02-06 10:03:09 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const botao = document.querySelector(".btn-plataforma");
-const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
-
-botao.addEventListener("click", () => {
-    
-    elementoPlataformas.classList.toggle("ativo");
-      
-   });
